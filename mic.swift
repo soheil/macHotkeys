870764f21f1afb190521183f8892bacb6ab70bf5
Soheil <ysoheil@gmail.com>
2023-08-10 16:18:18 -0700

    load env vars

--- mic.swift
+++ mic.swift
@@ -216,7 +216,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                         then
                             kill -2 $(ps aux | grep 'chat/run' | grep -v grep | awk '{print $2}')
                         else
-                            ~/chat/run --revert
+                            . ~/.bash_profile; ~/chat/run --revert
                         fi
                         """
 
