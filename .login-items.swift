3760f4c7ca5aabad067b18be176346f07023df4e
Soheil <ysoheil@gmail.com>
2023-08-14 06:25:23 -0700

    add login items

---
+++ .login-items.swift
@@ -0,0 +1,21 @@
+import Cocoa
+import Foundation
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let binaryPath = ProcessInfo.processInfo.arguments[0]
+        let dir = NSString(string: binaryPath).deletingLastPathComponent
+
+        let task = Process()
+        task.launchPath = "/usr/bin/env"
+        task.arguments = ["\(dir)/.login-items.sh"]
+        task.launch()
+        exit(0)
+    }
+}
+
+let app = NSApplication.shared
+let appDelegate = AppDelegate()
+
+app.delegate = appDelegate
+app.run()
