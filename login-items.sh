1d4bc6da0c7cc8cb9c203afaa7fed1d48054d3bb
Soheil <ysoheil@gmail.com>
2023-08-14 06:45:38 -0700

    rename files

---
+++ login-items.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+EDITOR_APP_NAME="Sublime Text"
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+osascript -e "quit app \"$EDITOR_APP_NAME\""
+
+while IFS= read -r line; do
+  launchctl setenv "${line%=*}" "${line#*=}"
+done < $DIR/.env
+
+
+open -a "$EDITOR_APP_NAME"
+
+$DIR/chat0/heater &
+$DIR/mic &
+
+while true; do
+  sleep 100
+done
